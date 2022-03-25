9efc85525eca7ef439d4124e001310446fa9d7f8
DoThao89 <93904825+DoThao89@users.noreply.github.com>
2022-03-25 17:53:23 +0700

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,9 @@
+package authentication;
+import  java.util.Scanner;
+public class Application {
+    public static void main(String[] args) throws Exception {
+        Controller controller = new Controller();
+        controller.home();
+    }
+}
+
